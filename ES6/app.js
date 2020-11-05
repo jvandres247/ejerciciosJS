219bcefed676946ba38b768d4aafd1934ad2f3ab
Anita Ceci <andres@canastarosa.com>
2020-11-05 14:59:44 -0600

    primer commit

---
+++ ES6/app.js
@@ -0,0 +1,143 @@
+//scope: el ambito e una variable (scope en ingles), es la zona del programa en la se define la variable
+//javascript define dos ambitos para las variables global y function
+
+//Toda variable en js tiene un valor por defecto: undefined (ems5)
+//var: cuando el interprete ve el archivo, busca una declaracion de variable y si esta aparece undefined
+//let: (ems6) obliga a que antes de ser usada la variable, esta exista
+//const: no puede variar o mutar de valor, es necesario inicializarla cuando se declara
+//Las constantes que son objetos, pueden cambiar sus propiedades perfectamente, pero no pueden ser igualadas directamente una vez declaradas.
+
+//en EMS6 solo puede haber una sola variable con el mismo nombre
+//¿Por que es mas seguro utilizar LET en lugar de VAR? R: Por que obliga a declarar variables antes de utilizarlas y evita la redeclaracion
+
+
+var funciones = [];
+
+for(var i=0; i<10; i++){
+    funciones.push( function() {
+        console.log(i);
+     } );
+}
+
+funciones.forEach( function(func){
+    func();
+})
+
+
+let numero = 10;
+ 
+if( true ){ 
+    let numero = 20;
+    
+    while(true){ 
+        let numero = 20; 
+        
+        for(let numero = 1; numero < 10; numero ++){ 
+            numero = numero; 
+        } break; 
+   }
+}
+ 
+console.log(numero);
+
+
+//Segementos de caracteres startsWith - endsWith - includes
+
+var saludo = "Hola Mundo";
+
+//EMS5
+console.log( saludo.substr(0,1) === "H"  );
+console.log( saludo.indexOf("x") );
+
+//EMS6
+console.log( saludo.startsWith("Hola") );
+console.log( saludo.endsWith("o") );
+console.log( saludo.includes("x") );
+
+
+console.log(saludo.startsWith("Mu", 5));
+console.log(saludo.includes("a", 5));
+
+//Repeticiones de strings - Repeat
+
+let texto = "Hola";
+
+console.log(texto.repeat(2));
+
+const ESPACIOS = 12;
+let nombres = ["Fernando", "Melissa", "Juan"];
+let numeros = ["9191112212", "1214442312", "12444454232"];
+
+for (i in nombres){
+    let dif = ESPACIOS - nombres[i].length;
+    console.log( nombres[i] + " ".repeat(dif)   + "|" + numeros[i] )
+}
+
+//Plantillas literales - Literal Templates
+
+let nombre = "Maria";
+let apellido = "Perez";
+
+let nombreCompleto = nombre + " " + apellido;
+
+console.log( nombreCompleto );
+
+let nombreCompleto2 = `El nombre completo es ${nombre} ${apellido}`
+console.log(nombreCompleto2)
+
+function obtenerNombre(){
+    return "Maria Perez";
+}
+
+let nombreCompleto3 = `El nombre completo es ${obtenerNombre()}`;
+console.log( nombreCompleto3 );
+
+
+//EMS5
+
+let multilinea = '<h1>Titulo</h1> \n<p>Hola Mundo</p>';
+console.log(multilinea)
+
+//EMS6
+let multilinea2 = `<h1 class="algo">${obtenerNombre()}</h1>
+<p>Hola Mundo</p>`;
+
+console.log(multilinea2)
+
+//Templates con tags
+function etiqueta( literales, ...substituciones ){
+    //console.log( arguments )
+    let resultado = "";
+
+    console.log(literales);
+    console.log(substituciones);
+
+    for(let i=0; i < substituciones.length; i++);
+    {
+        resultado += literales[i];
+        resultado += substituciones[i];
+    }
+    return resultado;
+}
+
+let unidades = 5,
+    costo_unitario = 10;
+
+let mensaje = etiqueta`${unidades} de lapices cuestan ${unidades * costo_unitario} pesos`;
+
+console.log(mensaje);
+
+//Usando valores "raw" (crudos) en templates literales
+
+let mensaje1 = `Hola \n mundo\\`,
+    mensaje2 = String.raw`Hola \n mundo\\`;
+
+console.log(mensaje1);
+console.log(mensaje2);
+
+//let numero = 10;
+//console.log(  numero.startsWith("1") );
+
+//startsWith junto con includes y endsWith, son funciones especiales de los STRINGS, 
+//y en este caso "numero" no es una cadena de caracteres.
+//las funciones son CaSeSeNsItIvE, es decir, que las mayúsculas y minúsculas son diferentes.
---
+++ ES6/app2.js
@@ -0,0 +1,154 @@
+//Parametros por defecto
+
+
+
+
+//Parametro REST
+
+/*
+    El parametro rest es indicado con tres puntos (...) seguido del nombre que se le asignara a 
+    dicho parametro
+
+    Ese parametro se convierte en un arreglo qeu contiene el "resto" de los parametros
+    pasados a la funcion
+
+    De ahi se origina el nombre REST
+*/
+/**
+arguments es un objeto similar a Array accesible
+dentro de funciones que contiene los valores de
+los argumentos pasados a esa función.
+ */
+//EMS5
+
+function agregar_alumno( ){
+    console.log( arguments );
+
+    for (var i=1; i<arguments.length; i++){
+        arguments[0].push(arguments[i]);
+    }
+
+    return arguments[0];
+}
+
+var alumnos_arr = ["Fernando"];
+var alumnos_arr2 = agregar_alumno(alumnos_arr, "Maria", "Pedro", "Susana");
+
+
+console.log(alumnos_arr2);
+
+//EMS6
+
+function agregar_alumno2(arr_alumnos, ...alumnos ){
+    console.log( arguments );
+
+    for (let i=0; i<alumnos.length; i++){
+        arr_alumnos.push(alumnos[i]);
+    }
+
+    return arr_alumnos;
+}
+
+var alumnos_arr = ["Fernando"];
+var alumnos_arr2 = agregar_alumno2(alumnos_arr, "Maria", "Pedro", "Susana");
+
+
+console.log(alumnos_arr2);
+
+//Restricciones del parametro REST
+
+/**
+ * 1- Solo puede existir un parametro rest en la funcion
+ * 2- El parametro rest debe de ir siempre como ultimo parametro
+ */
+
+//El operador SPREAD
+
+/**
+ * Mientras que el parametro REST permite especificar argumentos independientes que seran combinados
+    en un arreglo, el operador SPREAD permite especificar un arreglo que sera separado y cada item enviado
+    sera un argumento independiente a la funcion
+ */
+
+//EMS5
+var numeros = [1,2,3,4,5,6,7,8]
+var max = Math.max.apply(Math, numeros);
+console.log(max)
+//EMS6
+
+let nums = [1,2,3,4,5,6,7,8]
+let maxNum = Math.max(...nums)
+console.log(maxNum)
+
+//Romper la relación de referencia de los objetos
+let persona1= {
+    nombre: 'Fernando',
+    edad: 33
+}
+
+let persona2 = {...persona1};
+persona2.nombre = 'Juan'
+console.log(persona1);
+console.log(persona2);
+
+//Añadir propiedades a objetos a partir de otros objetos
+
+let persona3= {
+    nombre: 'Fernando',
+    edad: 33
+}
+
+let persona4 = {
+    nombre: 'Juan',
+    edad: 33,
+    direccion: 'Conocido 70110',
+    conduce: true,
+    vehiculo: true,
+    vegetariano: false,
+    casado: true,
+};
+
+persona3 = {
+    ...persona4,
+    ...persona3
+}
+
+console.log(persona3);
+console.log(persona4);
+
+// Diferencias entre el "Spread" y el "Rest"
+
+/**
+ * REST: Junta los elementos en un arreglo
+ * SPREAD: Esparce los elementos como si fueran enviados de forma separada
+ */
+
+function saludarRest(saludo, ...nombres){
+    for (i in nombres){
+        console.log(`${saludo} ${nombres[i]}`);
+    }
+}
+
+function saludarSpread(saludo, ...nombres) {
+    console.log(`${saludo} ${nombres}.`);
+}
+
+saludarRest("Hola", "Fernando", "Maria", "Susana");
+
+let personas = ["Melissa", "Hernando", "Juan"];
+
+saludarSpread("Que tal!", personas)
+
+let partes = ["brazos", "piernas"]
+let cuerpo = ["cabeza", "cuello", ...partes, "pies", "dedos"];
+
+console.log( cuerpo )
+
+// Aclarando el doble comportamiento de las funciones
+
+/**/
+//Los parametros se asignan en el mismo orden en el que son definidos en la función, por consecuencia, cuando mandamos "Matamoros", es asignado al nombre y no al apellido.
+//Un parámetro opcional, puede ser cualquier cosa que se le pueda enviar a una función por parámetro.
+//Arguments es un objeto de las funciones normales, que muestra todos los parámetros enviados a la función aunque no estén definidos explícitamente.
+//Argumens es un arreglo de los parametros que enviamos a la función al momento de ejecutarla. En este caso no estamos mandando ningún argumento o parámetro.
+//En el ECMAScript 5, no existía los parámetros rest, se puede tener la funcionalidad utilizando arguments, pero no es lo mismo.
